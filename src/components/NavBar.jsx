133c5e71184a8c06c538fe403da7c750ad620d99
rahul-ranjan-au8 <rahulranjan98@outlook.com>
2021-01-17 23:59:46 +0530

    bug-fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -132,7 +132,11 @@ function NavBar({ user }) {
               >
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
               </svg>
-
+              {/* <!--
+                Heroicon name: x
+  
+                Menu open: "block", Menu closed: "hidden"
+              --> */}
               <svg
                 className={`block h-6 w-6 ${isOpenMenu ? "block" : "hidden"}`}
                 xmlns="http://www.w3.org/2000/svg"
