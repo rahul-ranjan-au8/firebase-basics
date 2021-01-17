c99c9fcce45729f5e11d20a3da49e44403509b3b
rahul-ranjan-au8 <rahulranjan98@outlook.com>
2021-01-17 23:57:54 +0530

    bug-fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -73,16 +73,7 @@ function NavBar({ user }) {
                     />
                   </button>
                 </div>
-                {/* <!--
-                  Profile dropdown panel, show/hide based on dropdown state.
-  
-                  Entering: "transition ease-out duration-100"
-                    From: "transform opacity-0 scale-95"
-                    To: "transform opacity-100 scale-100"
-                  Leaving: "transition ease-in duration-75"
-                    From: "transform opacity-100 scale-100"
-                    To: "transform opacity-0 scale-95"
-                --> */}
+
                 <div
                   className={`origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-lg py-2 bg-white ring-1 ring-black ring-opacity-5 ${
                     isOpenProfile ? "block" : "hidden"
